a61c7077cc4b47b8005d8996d3ea0c3a6b146f2a
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2023-11-06 22:19:30 -0300

    UseCase: AuthenticateCompany

---
+++ gestao_vagas/src/main/java/br/com/brunosousa/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,21 @@
+package br.com.brunosousa.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.brunosousa.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.brunosousa.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class AuthCompanyUseCase {
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public void execute(AuthCompanyDTO authCompanyDTO) {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(() -> {
+            throw new UsernameNotFoundException("Company not found");
+        });
+
+    }
+}
